6d14e71688960d2daa127c596a332e37f0e600fd
Andri Jasinski <andrjuwkoy@gmail.com>
2017-12-06 13:20:46 +0200

    Initial commit and Merge sort algorithm

---
+++ src/main/java/algorithms/Algorithm.java
@@ -0,0 +1,7 @@
+package algorithms;
+
+import java.util.List;
+
+public abstract class Algorithm {
+    public abstract void start(List l);
+}
---
+++ src/main/java/main/Main.java
@@ -0,0 +1,28 @@
+package main;
+
+import algorithms.Algorithm;
+import algorithms.Merge;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Algorithm merger = new Merge();
+        for (int i = 1; i < 10; i++) {
+            List<Integer> l = new ArrayList<>();
+            Random generator = new Random();
+            for (int j = 0; j < i*10000; j++) {
+                l.add(generator.nextInt(5000));
+            }
+            long startTime = System.nanoTime();
+
+            merger.start(l);
+
+            long stopTime = System.nanoTime();
+            long elapsedTime = stopTime - startTime;
+            System.out.println("Mergesort " + elapsedTime + " . Num of elements " + i*10000);
+        }
+    }
+}
