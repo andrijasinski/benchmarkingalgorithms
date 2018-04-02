56bfbc4b5a923e1aa7468e78d587331cbdfd40f7
Andri Jasinski <jasinski@vedur.hpc.ut.ee>
2018-04-02 17:05:54 +0300

    script updates

--- slurm_scripts/buildProject.sh
+++ slurm_scripts/buildProject.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 #SBATCH --time=00:10:00
-#SBATCH -N 1
+#SBATCH -N 2
 #SBATCH --mem=2000
+
+N=2
 
 module load openmpi-2.1.0
 module load gradle/4.4
 
 echo "modules loaded"
+echo "NUMBER OF NODES = ${N}"
 cd ../benchmarkingalgorithms
 echo "starting build"
 gradle build
--- slurm_scripts/deleteOutput.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#SBATCH --time=00:10:00
-#SBATCH -N 1
-#SBATCH --mem=2000
-
-
-find . -name "*.out" -type f -delete
-
